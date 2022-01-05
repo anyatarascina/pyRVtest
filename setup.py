0dda48ff682d091b7436765d4c96b4ec21e7afc4
chrissullivanecon <96749787+chrissullivanecon@users.noreply.github.com>
2022-01-04 19:33:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     include_package_data=True,
-    version='0.1.0'
+    version='0.1.1'
 )
