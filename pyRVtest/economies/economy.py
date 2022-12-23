942770865a8d1fac2ce81e831c721b939f46f8c7
Anya Tarascina <anya.tarascina@gmail.com>
2022-12-23 13:16:31 -0500

    Remove unused methods from Economy. Remove tracking for tutorial generated files.

--- pyRVtest/economies/economy.py
+++ pyRVtest/economies/economy.py
@@ -4,19 +4,14 @@ import abc
 from typing import Any, Dict, Hashable, List, Mapping, Optional, Sequence, Tuple, Union
 
 import numpy as np
-from pyblp.utilities.algebra import precisely_identify_collinearity, precisely_identify_psd
-from pyblp.utilities.basics import (
-    Array, Error, RecArray, StringRepresentation, format_table, get_indices, output
-)
-from pyblp import exceptions
+from pyblp.utilities.basics import Array,  RecArray, StringRepresentation, format_table, get_indices
 
-from .. import options
 from ..configurations.formulation import Formulation, Absorb, ModelFormulation
 from ..primitives import Container
 
 
 class Economy(Container, StringRepresentation):
-    """An abstract economy underlying the BLP model."""
+    """An abstract economy underlying the firm conduct testing model."""
 
     model_formulations: Sequence[Optional[ModelFormulation]]
     cost_formulation: Formulation
@@ -100,7 +95,7 @@ class Economy(Container, StringRepresentation):
         return format_table(header, values, title="Dimensions")
 
     def _format_formulations(self) -> str:
-        """Formation information about the formulations of the economy as a string."""
+        """Format information about the formulations of the economy as a string."""
 
         # construct the data
         named_formulations = [(self._w_formulation, "w: Marginal Cost")]
@@ -120,7 +115,7 @@ class Economy(Container, StringRepresentation):
         return format_table(header, *data, title="Formulations")
 
     def _format_model_formulations(self) -> str:
-        """Formation information about the formulations of the economy as a string."""
+        """Format information about the model formulations as a string."""
 
         # construct the data
         data: List[List[str]] = []
@@ -142,120 +137,5 @@ class Economy(Container, StringRepresentation):
             data.append(["Markups Supplied by User"])
             header = [" "]
 
-        return format_table(header, *data, title="Models")    
+        return format_table(header, *data, title="Models")
 
-    def _detect_collinearity(self) -> None:
-        """Detect any collinearity issues in product data matrices."""
-
-        # skip collinearity checking when it is disabled via zero tolerances
-        if max(options.collinear_atol, options.collinear_rtol) <= 0:
-            return
-
-        # collect labels for columns of matrices that will be checked for collinearity issues
-        matrix_labels = {'w': [str(f) for f in self._w_formulation]}
-        for zz in range(len(self.Dict_Z_formulation)):
-            matrix_labels.update(
-                {"Z{0}".format(zz): [str(f) for f in self.Dict_Z_formulation["_Z{0}_formulation".format(zz)]]}
-            )
-            matrix_labels.update(
-                {"Z{0}".format(zz): [str(f) for f in self._w_formulation] + matrix_labels["Z{0}".format(zz)]}
-            )
-
-        # check each matrix for collinearity
-        for name, labels in matrix_labels.items():
-            collinear, successful = precisely_identify_collinearity(self.products[name])
-            common_message = "To disable collinearity checks, set options.collinear_atol = options.collinear_rtol = 0."
-            for zz in range(len(self.Dict_Z_formulation)):
-                if name in {'w', 'Z{0}'.format(zz)}:
-                    common_message = f"Absorbed fixed effects may be creating collinearity problems. {common_message}"
-            if not successful:
-                raise ValueError(
-                    f"Failed to compute the QR decomposition of {name} while checking for collinearity issues. "
-                    f"{common_message}"
-                )
-            if collinear.any():
-                collinear_labels = ", ".join(l for l, c in zip(labels, collinear) if c)
-                raise ValueError(
-                    f"Detected collinearity issues with [{collinear_labels}] and at least one other column in {name}. "
-                    f"{common_message}"
-                )
-
-    @staticmethod
-    def _detect_psd(matrix: Array, name: str) -> None:
-        """Detect whether a matrix is PSD."""
-        psd, successful = precisely_identify_psd(matrix)
-        common_message = "To disable PSD checks, set options.psd_atol = options.psd_rtol = numpy.inf."
-        if not successful:
-            raise ValueError(f"Failed to compute the SVD of {name} while checking that it is PSD. {common_message}")
-        if not psd:
-            raise ValueError(f"{name} must be a PSD matrix. {common_message}")
-
-    @staticmethod
-    def _handle_errors(errors: List[Error], error_behavior: str = 'raise') -> None:
-        """Either raise or output information about any errors."""
-        if errors:
-            if error_behavior == 'raise':
-                raise exceptions.MultipleErrors(errors)
-            output("")
-            output(exceptions.MultipleErrors(errors))
-            output("")
-
-    def _validate_product_ids(self, product_ids: Sequence[Any], market_ids: Optional[Array] = None) -> None:
-        """Validate that product IDs either contain None (denoting the outside option) or contain at least one product
-        ID for each market in the data (or in specific markets if specified). Also verify that each product ID appears
-        only once in each relevant market.
-        """
-        if self.unique_product_ids.size == 0 and any(i is not None for i in product_ids):
-            raise ValueError("Product IDs must have been specified.")
-
-        if market_ids is None:
-            market_ids = self.unique_market_ids
-
-        for t in market_ids:
-            counts = []
-            for product_id in product_ids:
-                count = 1
-                if product_id is not None:
-                    count = (self.products.product_ids[self._product_market_indices[t]] == product_id).sum()
-                    if count > 1:
-                        raise ValueError(
-                            f"Product IDs should be unique within markets, but ID '{product_id}' shows up {count} "
-                            f"times in market '{t}'."
-                        )
-                counts.append(count)
-
-            if all(c == 0 for c in counts):
-                raise ValueError(
-                    f"None of the product_ids {sorted(list(product_ids))} show up in market '{t}' with IDs: "
-                    f"{list(sorted(self.products.product_ids[self._product_market_indices[t]]))}."
-                )
-
-    def _coerce_optional_firm_ids(self, firm_ids: Optional[Any], market_ids: Optional[Array] = None) -> Array:
-        """Coerce optional array-like firm IDs into a column vector and validate it. By default, assume that firm IDs
-        are for all markets.
-        """
-        if firm_ids is None:
-            return None
-        firm_ids = np.c_[np.asarray(firm_ids, options.dtype)]
-        rows = self.N
-        if market_ids is not None:
-            rows = sum(i.size for t, i in self._product_market_indices.items() if t in market_ids)
-        if firm_ids.shape != (rows, 1):
-            raise ValueError(f"firm_ids must be None or a {rows}-vector.")
-        return firm_ids
-
-    def _coerce_optional_ownership(self, ownership: Optional[Any], market_ids: Optional[Array] = None) -> Array:
-        """Coerce optional array-like ownership matrices into a stacked matrix and validate it. By default, assume that
-        ownership matrices are for all markets.
-        """
-        if ownership is None:
-            return None
-        ownership = np.c_[np.asarray(ownership, options.dtype)]
-        rows = self.N
-        columns = self._max_J
-        if market_ids is not None:
-            rows = sum(i.size for t, i in self._product_market_indices.items() if t in market_ids)
-            columns = max(i.size for t, i in self._product_market_indices.items() if t in market_ids)
-        if ownership.shape != (rows, columns):
-            raise ValueError(f"ownership must be None or a {rows} by {columns} matrix.")
-        return ownership
