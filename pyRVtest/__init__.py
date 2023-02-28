cadd0ee9942bc297454bfa5de2ca36349bc34e64
Anya Tarascina <anya.tarascina@gmail.com>
2023-02-27 18:24:24 -0600

    STY: separate passthrough matrix into its own function

--- pyRVtest/__init__.py
+++ pyRVtest/__init__.py
@@ -2,13 +2,13 @@
 
 from . import data, options
 from .configurations.formulation import Formulation, ModelFormulation
-from .construction import (build_markups, compute_markups, read_pickle)
+from .construction import (build_markups, evaluate_first_order_conditions, read_pickle)
 from .economies.problem import Problem
 from .primitives import Models, Products
 from .results.problem_results import ProblemResults
 from .version import __version__
 
 __all__ = [
-    'data', 'options', 'read_pickle', 'build_markups', 'compute_markups', 'Formulation', 'ModelFormulation', 'Problem',
+    'data', 'options', 'read_pickle', 'build_markups', 'evaluate_first_order_conditions', 'Formulation', 'ModelFormulation', 'Problem',
     'Models', 'Products', 'ProblemResults', '__version__'
 ]
--- pyRVtest/construction.py
+++ pyRVtest/construction.py
@@ -14,7 +14,7 @@ from . import options
 
 
 def build_markups(
-        products: RecArray, demand_results: Mapping, model_downstream: Optional[Array],
+        product_data: RecArray, pyblp_results: Mapping, model_downstream: Optional[Array],
         ownership_downstream: Optional[Array], model_upstream: Optional[Array] = None,
         ownership_upstream: Optional[Array] = None, vertical_integration: Optional[Array] = None,
         custom_model_specification: Optional[dict] = None, user_supplied_markups: Optional[Array] = None) -> Array:
@@ -36,11 +36,11 @@ def build_markups(
 
     Parameters
     ----------
-    products : `recarray`
+    product_data : `recarray`
         The `product_data` containing information on markets and product characteristics. This should be the same as
         the data used for demand estimation. To compute markups, this data must include `prices`, `market_ids`, and
         `shares`.
-    demand_results : `structured array-like`
+    pyblp_results : `structured array-like`
         The results object obtained from using the pyBLP demand estimation procedure. We use built-in PyBLP
         functions to return the demand Jacobians and Hessians (first and second derivatives of shares with respect
         to prices).
@@ -75,12 +75,10 @@ def build_markups(
 
     """
 
-    # initialize
-    N = np.size(products.prices)
-    with contextlib.redirect_stdout(open(os.devnull, 'w')):
-        ds_dp = demand_results.compute_demand_jacobians()
+    # initialize market characteristics
+    N = product_data.shape[0]
     number_models = len(model_downstream)
-    markets = np.unique(products.market_ids)
+    markets = np.unique(product_data.market_ids)
 
     # initialize markups
     markups = [None] * number_models
@@ -90,6 +88,10 @@ def build_markups(
         markups_downstream[i] = np.zeros((N, 1), dtype=options.dtype)
         markups_upstream[i] = np.zeros((N, 1), dtype=options.dtype)
 
+    # precompute demand jacobians
+    with contextlib.redirect_stdout(open(os.devnull, 'w')):
+        ds_dp = pyblp_results.compute_demand_jacobians()
+
     # compute markups market-by-market
     for i in range(number_models):
         if user_supplied_markups[i] is not None:
@@ -97,43 +99,32 @@ def build_markups(
             markups_downstream[i] = user_supplied_markups[i]
         else:
             for t in markets:
-                index_t = np.where(demand_results.problem.products['market_ids'] == t)[0]
-                shares_t = products.shares[index_t]
+                index_t = np.where(pyblp_results.problem.products['market_ids'] == t)[0]
+                shares_t = product_data.shares[index_t]
                 retailer_response_matrix = ds_dp[index_t]
                 retailer_response_matrix = retailer_response_matrix[:, ~np.isnan(retailer_response_matrix).all(axis=0)]
 
-                # if there is an upstream model, compute demand hessians
-                if not (model_upstream[i] is None):
-                    with contextlib.redirect_stdout(open(os.devnull, 'w')):
-                        d2s_dp2_t = demand_results.compute_demand_hessians(market_id=t)
-
                 # compute downstream markups for model i market t
-                markups_downstream[i], retailer_ownership_matrix = compute_markups(
+                markups_downstream[i], retailer_ownership_matrix = evaluate_first_order_conditions(
                     index_t, model_downstream[i], ownership_downstream[i], retailer_response_matrix, shares_t,
                     markups_downstream[i], custom_model_specification[i], markup_type='downstream'
                 )
-                markups_t = markups_downstream[i][index_t]
 
                 # compute upstream markups (if applicable) following formula in Villas-Boas (2007)
                 if not (model_upstream[i] is None):
 
                     # construct the matrix of derivatives with respect to prices for other manufacturers
-                    J = len(shares_t)
-                    g = np.zeros((J, J))
-                    for j in range(J):
-                        g[j] = np.transpose(markups_t) @ (retailer_ownership_matrix * d2s_dp2_t[:, j, :])
-
-                    # solve for derivatives of all prices with respect to the wholesale prices
-                    H = np.transpose(retailer_ownership_matrix * retailer_response_matrix)
-                    G = retailer_response_matrix + H + g
-                    delta_p = inv(G) @ H
+                    markups_t = markups_downstream[i][index_t]
+                    passthrough_matrix = construct_passthrough_matrix(
+                        pyblp_results, t, retailer_response_matrix, retailer_ownership_matrix, markups_t
+                    )
 
                     # solve for matrix of cross-price elasticities of derived demand and the effects of cost
                     #   pass-through
-                    manufacturer_response_matrix = np.transpose(delta_p) @ retailer_response_matrix
+                    manufacturer_response_matrix = np.transpose(passthrough_matrix) @ retailer_response_matrix
 
                     # compute upstream markups
-                    markups_upstream[i], manufacturer_ownership_matrix = compute_markups(
+                    markups_upstream[i], manufacturer_ownership_matrix = evaluate_first_order_conditions(
                         index_t, model_upstream[i], ownership_upstream[i], manufacturer_response_matrix, shares_t,
                         markups_upstream[i], custom_model_specification[i], markup_type='upstream'
                     )
@@ -150,11 +141,32 @@ def build_markups(
     return markups, markups_downstream, markups_upstream
 
 
-def compute_markups(
+def construct_passthrough_matrix(
+        pyblp_results, market_id, retailer_response_matrix, retailer_ownership_matrix, markups_t):
+    """Construct the passthrough matrix using the formula from Villas-Boas (2007). This matrix contains the derivatives
+    of all retail prices with respect to all wholesale prices."""
+
+    # compute demand hessians
+    with contextlib.redirect_stdout(open(os.devnull, 'w')):
+        d2s_dp2_t = pyblp_results.compute_demand_hessians(market_id=market_id)
+
+    # compute the product of demand hessians and markups
+    J = len(markups_t)
+    g = np.zeros((J, J))
+    for j in range(J):
+        g[:, [j]] = (retailer_ownership_matrix * d2s_dp2_t[:, :, j]) @ markups_t
+
+    # solve for derivatives of all prices with respect to the wholesale prices
+    H = np.transpose(retailer_ownership_matrix * retailer_response_matrix)
+    G = retailer_response_matrix + H + g
+    return inv(G) @ H
+
+
+def evaluate_first_order_conditions(
         index, model_type, type_ownership_matrix, response_matrix, shares, markups, custom_model_specification,
         markup_type):
-    """Compute markups for some standard models including Bertrand, Cournot, monopoly, and perfect competition. Allow
-    user to pass in their own markup function as well.
+    """Compute markups for some standard models including Bertrand, Cournot, monopoly, and perfect competition using
+    the first order conditions corresponding to each model. Allow user to pass in their own markup function as well.
     """
     if (markup_type == 'downstream') or (markup_type == 'upstream' and model_type is not None):
 
@@ -162,7 +174,7 @@ def compute_markups(
         ownership_matrix = type_ownership_matrix[index]
         ownership_matrix = ownership_matrix[:, ~np.isnan(ownership_matrix).all(axis=0)]
 
-        # compute markups based on specified model
+        # compute markups based on specified model first order condition
         if model_type == 'bertrand':
             markups[index] = -inv(ownership_matrix * response_matrix) @ shares
         elif model_type == 'cournot':
